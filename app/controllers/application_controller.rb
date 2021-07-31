712602923d29a87a3cbfae980503497a6ff8df98
Mariana <marianamillicovsky@gmail.com>
2021-07-31 00:35:24 -0300

    not finished yet. Changes done after 30/07/2021.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::API
 
         protected
         def configure_permitted_parameters
-          devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password])
+          devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :registration])
         end        
 end
 
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+    :user_name => 'API RoR Disney', # This is the string literal 'apikey', NOT the ID of your API key
+    :password => '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', # This is the secret sendgrid API key which was issued during API key creation
+    :domain => 'disneyexplorer.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+  
